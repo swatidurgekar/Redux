db0edb0140363e9530c52bc903d121ccc2bc894f
swatidurgekar <swatidurgekar@gmail.com>
2023-06-08 10:29:21 +0530

    first commit

---
+++ redux-demo.js
@@ -0,0 +1,26 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  } else {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
